09c2c4d68b6654209141007fb0cb57e839b91199
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2023-03-26 18:48:39 -0300

    feat: added functions (get all holidays and get holiday by date)

--- src/HolidaysCollection.java
+++ src/HolidaysCollection.java
@@ -3,6 +3,8 @@ import java.util.List;
 public class HolidaysCollection {
   private List<Holiday> holidaysList;
 
+  public HolidaysCollection() {}
+
   public HolidaysCollection(List<Holiday> holidaysList) {
     this.holidaysList = holidaysList;
   }
@@ -11,9 +13,34 @@ public class HolidaysCollection {
     return this.holidaysList;
   }
 
-  public void getHolidayByDate(String dateAsString) {
-    // procurar na lista de feriados um que contenha a data igual à do parâmetro passado
+  public void setHolidays(List<Holiday> holidays) {
+    this.holidaysList = holidays;
+  }
 
+  public void add(Holiday newHoliday) {
+    this.holidaysList.add(newHoliday);
+  }
+
+  public int size() {
+    return this.holidaysList.size();
+  }
+
+  public void getHolidayByDate(String dateAsString) {
+    for (int i = 0; i < this.size(); i++) {
+      Holiday actualHoliday = this.holidaysList.get(i);
+      if(actualHoliday.getDate().equals(dateAsString)) {
+        System.out.println("Feriado encontrado: " + actualHoliday.getDate() + " => " + actualHoliday.getDescription());
+        return;
+      }
+    }
+    System.out.println("Não existe feriado na data informada: (" + dateAsString + ")");
+  }
+
+  public void getAllHolidays() {
+    System.out.println("\t -- Lista completa de feriados: --");
+    for (int i = 0; i < this.holidaysList.size(); i++) {
+      System.out.println(this.holidaysList.get(i));
+    }
   }
 
   
