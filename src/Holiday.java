38cd1b4af6d6bbda3c72090d3cdfbb90bb6f8cce
Tarcísio F. S. Teles <tfst@ecomp.poli.br>
2023-03-26 18:47:20 -0300

    feat: added empty constructor

--- src/Holiday.java
+++ src/Holiday.java
@@ -2,6 +2,8 @@ public class Holiday {
   private String date;
   private String description;
   
+  public Holiday() {}
+  
   public Holiday(String date, String description) {
     this.date = date;
     this.description = description;
@@ -21,6 +23,11 @@ public class Holiday {
 
   public void setDescription(String description) {
     this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return this.date + ": " + this.description;
   }
  
 }
